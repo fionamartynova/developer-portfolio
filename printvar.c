a40a2fa0c80f7ce10395c8a8feec81ffe891e4cc
fionamartynova <fiona.victorovna@gmail.com>
2024-10-01 19:39:57 +0300

    demonstration of variable assignment and output

---
+++ printvar.c
@@ -0,0 +1,10 @@
+#include <stdio.h> //#1
+int main()
+{
+    int min = -70;
+    double volume = -22.43;
+    const double g = 406.01;
+    printf("min=%d volume=%.2f g=%.2f", min, volume, g);
+
+    return 0;
+}
